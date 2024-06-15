a979c7950d3d0931c4e55c116002cbb1d033518b
abdvlla <abood67200@gmail.com>
2024-06-15 09:45:22 -0300

    updated Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,27 +3,6 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Login = ({ onLogin }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/login",
-        { username, password },
-        { withCredentials: true } // Ensure cookies are included
-      );
-      if (response.status === 200) {
-        navigate("/");
-        onLogin();
-      }
-    } catch (error) {
-      console.error("Invalid credentials", error);
-    }
-  };
-
   return (
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-gray-50">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
@@ -31,14 +10,12 @@ const Login = ({ onLogin }) => {
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
             Sign in to your account
           </h1>
-          <form onSubmit={handleLogin} className="space-y-4 md:space-y-6">
+          <form className="space-y-4 md:space-y-6">
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">
                 Your email
               </label>
               <input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@company.com"
                 required={true}
@@ -51,8 +28,6 @@ const Login = ({ onLogin }) => {
               </label>
               <input
                 type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 placeholder="••••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required={true}
