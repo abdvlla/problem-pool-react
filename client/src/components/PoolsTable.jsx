a0b9ef8bd4f8cd6ddaa8c203a992cf21bdb2091a
abdvlla <abood67200@gmail.com>
2024-06-22 09:24:50 -0300

    Added clear search button

--- client/src/components/PoolsTable.jsx
+++ client/src/components/PoolsTable.jsx
@@ -145,6 +145,11 @@ const PoolsTable = ({ pools, onBulkUpdate }) => {
     localStorage.removeItem("searchQuery");
   };
 
+  const clearSearch = () => {
+    setSearchQuery("");
+    localStorage.removeItem("searchQuery");
+  };
+
   const handleCheckboxChange = (e, poolId) => {
     if (e.target.checked) {
       setSelectedPools([...selectedPools, poolId]);
@@ -288,22 +293,44 @@ const PoolsTable = ({ pools, onBulkUpdate }) => {
               onChange={handleSearchChange}
               value={searchQuery}
             />
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 1 0-4 4m0 0a7 7 0 0 0 4-4z"
-                ></path>
-              </svg>
-            </div>
+
+            {searchQuery === "" ? (
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-500"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 1 0-4 4m0 0a7 7 0 0 0 4-4z"
+                  ></path>
+                </svg>
+              </div>
+            ) : (
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-800 dark:text-gray-200">
+                <button onClick={clearSearch}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
